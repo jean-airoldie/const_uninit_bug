9a76ceddf84e25e00242ff70a6f43e30aeca0bcf
jean-airoldie <25088801+jean-airoldie@users.noreply.github.com>
2024-10-11 17:52:34 -0400

    Update example for clarity

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+#![feature(const_ptr_write)]
+
 use static_assertions::assert_eq_size;
 
 struct Buf<const N: usize> {
@@ -13,46 +15,33 @@ impl<const N: usize> Buf<N> {
         }
     }
 
-    const fn push_u32_slice(&mut self, slice: &[u32]) {
+    const fn push_frame_slice(&mut self, slice: &[Frame]) {
         if self.bytes.len() - self.cursor < slice.len() {
             panic!("exceeded capacity");
         }
 
         let mut i = 0;
         while i < slice.len() {
-            let array = slice[i].to_le_bytes();
-            let mut j = 0;
-            while j < slice.len() {
-                self.bytes[self.cursor + i] = array[j];
-                j += 1;
-            }
+            let ptr = unsafe { self.bytes.as_mut_ptr().add(self.cursor) as *mut Frame };
+            unsafe { ptr.write(slice[i]) };
+            self.cursor += std::mem::size_of::<Frame>();
             i += 1;
         }
         self.cursor += slice.len();
     }
 }
 
+#[derive(Copy, Clone)]
 #[repr(u8)]
 enum Frame {
     First(u16),
     Second,
 }
 
-assert_eq_size!(Frame, u32);
-
-impl Frame {
-    const fn cast_slice(slice: &[Frame]) -> &[u32] {
-        // SAFETY We know the slice is valid and casting to bytes should
-        // always be valid, even if repr(rust) isn't stable yet.
-        unsafe { std::mem::transmute(slice) }
-    }
-}
-
 const FRAMES: &[Frame] = &[Frame::First(8), Frame::Second];
 const NB_BYTES: usize = FRAMES.len() * std::mem::size_of::<Frame>();
 const SERIALIZED_FRAMES: [u8; NB_BYTES] = {
     let mut buf = Buf::<NB_BYTES>::new();
-    let bytes = Frame::cast_slice(FRAMES);
-    buf.push_u32_slice(&bytes);
+    buf.push_frame_slice(FRAMES);
     buf.bytes
 };
